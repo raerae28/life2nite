595f521d9ace66efec32477a1e1b69041cc512bd
Rae Williams <raewilliams@gmail.com>
2013-02-13 17:55:00 -0500

    First commit of application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Life2nite::Application.config.secret_token = 'c73d70a801e96fcf62612563b580413386db7d6d44d1a445b2fa919c147c8ce7db950536b1c2d0960cbc3ae0890bc019061537bba5dd4b874c6d17a65c3bb406'
